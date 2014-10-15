2efb4706d4ec92ac1b13bcdfd9d4377e09a065fc
ulnda <denis.nagibin@gmail.com>
2014-10-15 12:13:35 +0400

    Authentication shared_examples and access control test

--- spec/requests/users_pages_spec.rb
+++ spec/requests/users_pages_spec.rb
@@ -3,22 +3,40 @@ require 'spec_helper'
 describe 'Users' do
 
 	describe 'Contacts' do
-		context 'with contacts' do
-			before do 
-				@contact = create(:contact)
-				post api_v1_users_contacts_path phones: [@contact.phone]
-			end
-
-			it { expect(response.body).to include(@contact.phone) }
+		before do
+			@contact = build(:contact)
 		end
 
-		context 'without contacts' do
-			before do 
-				@contact = build(:contact)
-				post api_v1_users_contacts_path, phones: [@contact.phone]
+		context 'after authentication' do
+			before do
+				user = create(:user)
+				sign_in(user)
 			end
 
-			it { expect(response.body).not_to include(@contact.phone) }
+			context 'with contacts' do
+				before do 
+					@contact.save
+					post api_v1_users_contacts_path phones: [@contact.phone]
+				end
+
+				it { expect(response.body).to include(@contact.phone) }
+			end
+
+			context 'without contacts' do
+				before do 
+					post api_v1_users_contacts_path, phones: [@contact.phone]
+				end
+
+				it { expect(response.body).not_to include(@contact.phone) }
+			end
+		end
+
+		context 'before authentication' do
+			before do
+				post api_v1_users_contacts_path, phones: [@contact.phone]				
+			end
+
+			it_behaves_like 'access to system without authentication'
 		end
 	end
 end
